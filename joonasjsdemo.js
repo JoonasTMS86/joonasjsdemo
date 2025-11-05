0adc0aedf4d18af2c68ec071bf11f6ba1772d41e
Joonas Lindberg <joonaslindberg@hotmail.com>
2025-11-05 14:55:12 +0200

    Initial commit

---
+++ joonasjsdemo.js
@@ -0,0 +1,73 @@
+const screenWidth = 1910;
+const screenHeight = 909;
+const rowStride = screenWidth * 4;
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var imgData;
+
+let Application = PIXI.Application,
+	Container = PIXI.Container,
+	loader = PIXI.loader,
+	resources = PIXI.loader.resources,
+	TextureCache = PIXI.utils.TextureCache,
+	Sprite = PIXI.Sprite;
+let app = new Application(
+{
+	width: screenWidth, 
+	height: screenHeight,
+	antialiasing: false, 
+	transparent: false, 
+	resolution: 1,
+	forceCanvas: true
+}
+);
+loader
+	.load(setup);
+
+function setup() 
+{
+	state = play;
+	//Start the game loop
+	app.ticker.add(delta => gameLoop(delta));
+}
+
+function updateStatus()
+{
+	requestAnimationFrame(updateStatus);
+}
+
+function gameLoop(delta)
+{
+	//Update the current game state
+	state(delta);
+}
+
+async function doSpriteTransparency(givenbufferctx, givenbuffer, givenpic, keyR, keyG, keyB)
+{
+	const sizeofit = 4 * givenbuffer.width * givenbuffer.height;
+	for(var tpPos = 0; tpPos < sizeofit; tpPos += 4)
+	{
+		if(givenpic.data[tpPos] == keyR && givenpic.data[tpPos+1] == keyG && givenpic.data[tpPos+2] == keyB) givenpic.data[tpPos+3] = 0;
+	}
+	givenbufferctx.putImageData(givenpic, 0, 0);
+}
+
+window.onload = function() {
+	imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for(var y = 0; y < screenHeight; y++) {
+        for(var x = 0; x < screenWidth; x++) {
+            imgData.data[(y * rowStride) + (x * 4) + 0] = 255;
+            imgData.data[(y * rowStride) + (x * 4) + 1] = 255;
+            imgData.data[(y * rowStride) + (x * 4) + 2] = 255;
+            imgData.data[(y * rowStride) + (x * 4) + 3] = 255;
+        }
+    }
+};
+
+function play(delta)
+{
+    if(imgData != null) ctx.putImageData(imgData, 0, 0);
+	imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+}
